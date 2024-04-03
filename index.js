1a101439725e0f26b4b825b1aca94469c10fe649
jordan barani <jordan.barani@student.moringaschool.com>
2024-04-03 12:14:00 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(arrayOfObjects) {
+    const winObject = arrayOfObjects.find(obj => obj.result === "W");
+    return winObject ? winObject.year : undefined;
+}
+const superBowls = [
+    { year: 2019, result: "L" },
+    { year: 2020, result: "W" },
+    { year: 2021, result: "L" }
+];
+
+console.log(superbowlWin(superBowls)); // Output: 2020
